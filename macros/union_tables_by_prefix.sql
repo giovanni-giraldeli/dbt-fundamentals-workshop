49829bc6f0d074aa6a0badd988558d6fbfaa008e
Giovanni Giraldeli <giovanni.giraldeli@gmail.com>
2025-03-28 10:34:46 +0000

    Reverting dbt_utils macro call

--- macros/union_tables_by_prefix.sql
+++ macros/union_tables_by_prefix.sql
@@ -1,6 +1,6 @@
-{%- macro union_relations_by_prefix(database, schema, prefix) -%}
+{%- macro union_tables_by_prefix(database, schema, prefix) -%}
 
-    {%- set tables=dbt_utils.get_relations_by_prefix(DATABASE=database, SCHEMA=schema, PREFIX=prefix) -%}
+    {%- set tables=dbt_utils.get_relations_by_prefix(database=database, schema=schema, prefix=prefix) -%}
     
         {% for table in tables %}
         
